1b2892d3925e0fa523cc76cb990b2c4b52e4650c
Jakub Motysia <jakub.motysia@epita.fr>
2025-12-05 10:39:38 +0100

    added services

--- back-end/services/bar_service.py
+++ back-end/services/bar_service.py
@@ -1,3 +1,10 @@
-print("bar_service layer file initialized")
+from models.bar import Bar
 
-print("test commit")
+def get_all_bars():
+    return Bar.find_all()
+
+def get_bar_by_id(Bar_id):
+    return Bar.find_by_id(Bar_id)
+
+def get_bar_by_name(name):
+    return Bar.fin_by_name(name)
