533ab35657b81a621e3f6662bb3dc1002715b3d2
ivan.bomko <vanya.bomko@gmail.com>
2025-12-07 09:09:00 +0100

    add routes

---
+++ back-end/routes/filter_route.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from controllers.bar_filter_controller import FilterController
+
+filter_bp = Blueprint('filters', __name__)
+
+@filter_bp.route('/cities', methods=['GET'])
+def get_bars():
+    return FilterController.get_bars()
+
+@filter_bp.route('/stats', methods=['GET'])
+def get_statistics():
+    return FilterController.get_statistics()
+
+@filter_bp.route('/arrondissement/<arrondissement>', methods=['GET'])
+def get_bars_by_arrondissement(arrondissement):
+    return FilterController.get_bars_by_arrondissement(arrondissement)
